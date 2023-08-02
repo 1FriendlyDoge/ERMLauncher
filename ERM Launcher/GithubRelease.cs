769c21db1bc179537dd0da5e3aeb1a3cb2059f1a
MaximusGaming <maximilian@gronenthal.de>
2023-08-02 19:15:03 +0200

    Initial commit

---
+++ ERM Launcher/GithubRelease.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ERM_Launcher;
+
+public class Asset
+{
+    public string url { get; set; } = String.Empty;
+    public int id { get; set; }
+    public string node_id { get; set; } = String.Empty;
+    public string name { get; set; } = String.Empty;
+    public string label { get; set; } = String.Empty;
+    public Uploader uploader { get; set; } = new();
+    public string content_type { get; set; } = String.Empty;
+    public string state { get; set; } = String.Empty;
+    public int size { get; set; }
+    public int download_count { get; set; }
+    public DateTime created_at { get; set; }
+    public DateTime updated_at { get; set; }
+    public string browser_download_url { get; set; } = String.Empty;
+}
+
+public class Author
+{
+    public string login { get; set; } = String.Empty;
+    public int id { get; set; }
+    public string node_id { get; set; } = String.Empty;
+    public string avatar_url { get; set; } = String.Empty;
+    public string gravatar_id { get; set; } = String.Empty;
+    public string url { get; set; } = String.Empty;
+    public string html_url { get; set; } = String.Empty;
+    public string followers_url { get; set; } = String.Empty;
+    public string following_url { get; set; } = String.Empty;
+    public string gists_url { get; set; } = String.Empty;
+    public string starred_url { get; set; } = String.Empty;
+    public string subscriptions_url { get; set; } = String.Empty;
+    public string organizations_url { get; set; } = String.Empty;
+    public string repos_url { get; set; } = String.Empty;
+    public string events_url { get; set; } = String.Empty;
+    public string received_events_url { get; set; } = String.Empty;
+    public string type { get; set; } = String.Empty;
+    public bool site_admin { get; set; }
+}
+
+public class GithubRelease
+{
+    public string url { get; set; } = String.Empty;
+    public string assets_url { get; set; } = String.Empty;
+    public string upload_url { get; set; } = String.Empty;
+    public string html_url { get; set; } = String.Empty;
+    public int id { get; set; }
+    public Author author { get; set; } = new();
+    public string node_id { get; set; } = String.Empty;
+    public string tag_name { get; set; } = String.Empty;
+    public string target_commitish { get; set; } = String.Empty;
+    public string name { get; set; } = String.Empty;
+    public bool draft { get; set; }
+    public bool prerelease { get; set; }
+    public DateTime created_at { get; set; }
+    public DateTime published_at { get; set; }
+    public List<Asset> assets { get; set; } = new();
+    public string tarball_url { get; set; } = String.Empty;
+    public string zipball_url { get; set; } = String.Empty;
+    public string body { get; set; } = String.Empty;
+}
+
+public class Uploader
+{
+    public string login { get; set; } = String.Empty;
+    public int id { get; set; }
+    public string node_id { get; set; } = String.Empty;
+    public string avatar_url { get; set; } = String.Empty;
+    public string gravatar_id { get; set; } = String.Empty;
+    public string url { get; set; } = String.Empty;
+    public string html_url { get; set; } = String.Empty;
+    public string followers_url { get; set; } = String.Empty;
+    public string following_url { get; set; } = String.Empty;
+    public string gists_url { get; set; } = String.Empty;
+    public string starred_url { get; set; } = String.Empty;
+    public string subscriptions_url { get; set; } = String.Empty;
+    public string organizations_url { get; set; } = String.Empty;
+    public string repos_url { get; set; } = String.Empty;
+    public string events_url { get; set; } = String.Empty;
+    public string received_events_url { get; set; } = String.Empty;
+    public string type { get; set; } = String.Empty;
+    public bool site_admin { get; set; }
+}
+
---
+++ ERM Launcher/MainWindow.axaml.cs
@@ -0,0 +1,227 @@
+using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Handlers;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using Avalonia.Controls;
+using Avalonia.Threading;
+using Newtonsoft.Json;
+
+namespace ERM_Launcher;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+        Focus();
+    }
+
+    public static long expectedBytes;
+    public static DateTime lastPull = DateTime.Now;
+    
+    private async void StyledElement_OnInitialized(object? sender, EventArgs e)
+    {
+        HttpClientHandler handler = new HttpClientHandler() { AllowAutoRedirect = true };
+        ProgressMessageHandler progress = new ProgressMessageHandler(handler);
+
+        progress.HttpReceiveProgress += (_, args) =>
+        {
+            double progressPercentage = args.ProgressPercentage;
+            
+            if((int)progressPercentage == (int)ProgressIndicator.Value || lastPull.Add(TimeSpan.FromMilliseconds(100)) > DateTime.Now)
+            {
+                return;
+            }
+
+            lastPull = DateTime.Now;
+            string totalMb = ((double)expectedBytes / 1024 / 1024).ToString("0.0") + "MB";
+            string receivedMb = ((double)args.BytesTransferred / 1024 / 1024).ToString("0.0") + "MB";
+            
+            Dispatcher.UIThread.Post(() =>
+            {
+                ProgressIndicator.Value = progressPercentage;
+                StatusIndicator.Text = $"Downloading {progressPercentage}% - {receivedMb}/{totalMb}...";
+            });
+        };
+        
+        HttpClient fetchClient = new HttpClient(handler);
+        fetchClient.DefaultRequestHeaders.Add("User-Agent", "request");
+        
+        HttpClient downloadClient = new HttpClient(progress);
+        fetchClient.DefaultRequestHeaders.Add("User-Agent", "request");
+        
+        string filePath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+        string ermDir = Path.Join(filePath, "ERM");
+
+        DateTime lastChanged = DateTimeOffset.FromUnixTimeSeconds(0).DateTime;
+        
+        if(!Directory.Exists(ermDir))
+        {
+            Directory.CreateDirectory(ermDir);
+        }
+
+        if(RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        {
+            if(File.Exists(Path.Join(ermDir, "ERM.Desktop.exe")))
+            {
+                lastChanged = File.GetLastWriteTimeUtc(Path.Join(ermDir, "ERM.Desktop.exe"));
+            }
+        }
+        
+        if(RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+        {
+            if(File.Exists(Path.Join(ermDir, "ERM Desktop")))
+            {
+                lastChanged = File.GetLastWriteTimeUtc(Path.Join(ermDir, "ERM Desktop"));
+            }
+        }
+
+        HttpResponseMessage fetchResponse = await fetchClient.GetAsync("https://api.github.com/repos/1FriendlyDoge/Desktop-Releases/releases/latest");
+
+        
+        if(fetchResponse.StatusCode == HttpStatusCode.TooManyRequests)
+        {
+            await Dispatcher.UIThread.InvokeAsync(() =>
+            {
+                StatusIndicator.Text = "GitHub rate limit exceeded. Launching anyways.";
+            });
+            
+            await LaunchApp(ermDir);
+
+            await Task.Delay(3000);
+            Environment.Exit(0);
+        }
+        else if(!fetchResponse.IsSuccessStatusCode)
+        {
+            Environment.Exit(1);
+        }
+        
+        GithubRelease? githubRelease = JsonConvert.DeserializeObject<GithubRelease>(await fetchResponse.Content.ReadAsStringAsync());
+        
+        if(githubRelease == null)
+        {
+            Environment.Exit(1);
+        }
+        
+        if(RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        {
+            Asset? asset = githubRelease.assets.Find(x => x.name == "ERM.Desktop.exe");
+            
+            if(asset != null && lastChanged.ToUniversalTime() < githubRelease.published_at.ToUniversalTime())
+            {
+                expectedBytes = asset.size;
+                
+                HttpResponseMessage downloadResponse = await downloadClient.GetAsync(asset.browser_download_url);
+                Task<byte[]> receiveTask = downloadResponse.Content.ReadAsByteArrayAsync();
+                byte[] data = await receiveTask;
+                    
+                if(receiveTask.IsCompletedSuccessfully)
+                {
+                    try
+                    {
+                        await File.WriteAllBytesAsync(Path.Join(ermDir, "ERM.Desktop.exe"), data);
+                    }
+                    catch(Exception)
+                    {
+                        // ignored
+                    }
+                    
+                }
+            }
+        }
+        else if(RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+        {
+            Asset? asset = githubRelease.assets.Find(x => x.name == "ERM.Desktop.MacOS.zip");
+            
+            if(asset != null && lastChanged.ToUniversalTime() < githubRelease.published_at.ToUniversalTime())
+            {
+                expectedBytes = asset.size;
+                
+                HttpResponseMessage downloadResponse = await downloadClient.GetAsync(asset.browser_download_url);
+                Task<byte[]> receiveTask = downloadResponse.Content.ReadAsByteArrayAsync();
+                byte[] data = await receiveTask;
+                    
+                if(receiveTask.IsCompletedSuccessfully)
+                {
+                    try
+                    {
+                        await File.WriteAllBytesAsync(Path.Join(ermDir, "ERM.Desktop.MacOS.zip"), data);
+                    }
+                    catch(Exception)
+                    {
+                        // ignored
+                    }
+
+                    Dispatcher.UIThread.Post(() =>
+                    {
+                        StatusIndicator.Text = "Extracting...";
+                    });
+
+                    await Task.Run(() =>
+                    {
+                        System.IO.Compression.ZipFile.ExtractToDirectory(Path.Join(ermDir, "ERM.Desktop.MacOS.zip"), ermDir, true);
+                    });
+                    
+                    if(File.Exists(Path.Join(ermDir, "ERM Desktop")))
+                    {
+                        ProcessStartInfo startInfo = new ProcessStartInfo() 
+                        {
+                            FileName = "/bin/bash",
+                            Arguments = $"-c \" chmod +x  {Path.Join(ermDir, "ERM Desktop")}\" ",
+                            CreateNoWindow = true
+                        };
+
+                        new Process() { StartInfo = startInfo }.Start();
+                    }
+
+                    File.Delete(Path.Join(ermDir, "ERM.Desktop.MacOS.zip"));
+                }
+            }
+        }
+        
+        await Dispatcher.UIThread.InvokeAsync(() =>
+        {
+            ProgressIndicator.Value = 100;
+            StatusIndicator.Text = "Launching...";
+        });
+        
+        await LaunchApp(ermDir);
+        
+        await Task.Delay(3000);
+        
+        Environment.Exit(0);
+    }
+    
+    public async Task LaunchApp(string parentDir)
+    {
+        await Task.Run(() =>
+        {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                if(File.Exists(Path.Join(parentDir, "ERM.Desktop.exe")))
+                {
+                    Process.Start(new ProcessStartInfo(Path.Join(parentDir, "ERM.Desktop.exe"))
+                        { UseShellExecute = true });
+                }
+            }
+            else if(RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+            {
+                if(File.Exists(Path.Join(parentDir, "ERM Desktop")))
+                {
+                    Process.Start(new ProcessStartInfo(Path.Join(parentDir, "ERM Desktop"))
+                        { UseShellExecute = true });
+                }
+            }
+        });
+    }
+
+    private void Window_OnClosing(object? sender, CancelEventArgs e)
+    {
+        Environment.Exit(0);
+    }
+}
