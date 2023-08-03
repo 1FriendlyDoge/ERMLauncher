4d1292a5122e6c634869cd5321d26a27d6981d01
MaximusGaming <maximilian@gronenthal.de>
2023-08-03 02:57:50 +0200

    fix

--- ERM Launcher/MainWindow.axaml.cs
+++ ERM Launcher/MainWindow.axaml.cs
@@ -208,12 +208,7 @@ public partial class MainWindow : Window
         {
             if(File.Exists(Path.Join(parentDir, "ERM Desktop.app")))
             {
-                ProcessStartInfo startInfo = new ProcessStartInfo("open", $"-a {Path.Join(parentDir, "ERM Desktop.app")}");
-
-                Process process = new Process();
-                process.StartInfo = startInfo;
-                process.Start();
-                process.WaitForExit();
+                Process.Start(new ProcessStartInfo(Path.Join(parentDir, "ERM Desktop.app", "Contents", "MacOS", "ERM Desktop")));
             }
         }
     }
